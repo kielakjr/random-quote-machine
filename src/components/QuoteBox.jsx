9b5b1ba29e4617db04e69fc524c9b655aa62a003
mkielak <kielakowsky17@gmail.com>
2025-10-18 14:28:32 +0200

    Added function to fetchQuotes

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+const API_URL = "https://api.quotable.io/random";
+const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
 
 const QuoteBox = () => {
-  return (
-    <div className="quote-box">QuoteBox</div>
+    const [quote, setQuote] = useState("");
+
+    const fetchQuote = async () => {
+        try {
+            const response = await fetch(API_URL, API_OPTIONS);
+            const data = await response.json();
+            setQuote(data.content);
+        } catch (error) {
+            console.error("Error fetching the quote: ", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchQuote();
+    }, []);
+
+    return (
+    <div className="quote-box" id="quote-box">{quote}</div>
   )
 }
 
